798d8b7d97064292c96722c098cf78b3faf327fa
SAM VAN DEN BOSCH <samvandenbosch2003@gmail.com>
2020-03-23 11:33:19 +0100

    lvl 1 opdr 6

--- bucket_list_friends.php
+++ bucket_list_friends.php
@@ -1,23 +1,23 @@
 <?php
 
-$aantal = readline("hoeveel vrienden zal ik vragen om hun droom?");
+echo "hoeveel vrienden zal ik om hun droom vragen?" . "\n";
+$aantal = readline("");
 
 if (is_numeric($aantal)) {
     $bucket = array();
-        for($i=1; $i <= $aantal; $i++){
-
-            $namen = readline("wat is je naam?");
-            $dromen = readline("wat is jou droom?");
-            $totaal[$namen] = $dromen;
-        }
-
-
+    for ($i=1; $i <= $aantal; $i++) {
+        echo "wat is jou naam?" . "\n";
+        $namen = readline(""); 
+        echo "wat is jou droom?" . "\n";
+        $dromen = readline("");
+        $totaal[$namen] = $dromen;
+    }
+    
     foreach ($totaal as $namen1 => $dromen1) {
-
-         echo "$namen1 heeft als droom $dromen1" . "\n";
+        echo "$namen1 heeft als droom $dromen1" . "\n";
     }
 
-}else{
+} else {
     echo "dit is geen getal";
 }
 
